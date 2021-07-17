5923b34afb4341444c28d6cc0c56496783549d41
chytran <chytran@users.noreply.github.com>
2021-07-17 11:02:58 -0700

    adding footer and header php

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Login Project</title>
+    <!--================== CSS =====================-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="nav" id="nav">
+        <ul class="nav__menu">
+            <li class="nav__item"><a href="index.php" class="nav__link">Home</a></li>
+            <li class="nav__item"><a href="discover.php" class="nav__link">About Us</a></li>
+            <li class="nav__item"><a href="blog.php" class="nav__link">Find Blogs</a></li>
+            <li class="nav__item"><a href="signup.php" class="nav__link">Sign up</a></li>
+            <li class="nav__item"><a href="login.php" class="nav__link">Log in</a></li>
+        </ul>
+    </nav>
