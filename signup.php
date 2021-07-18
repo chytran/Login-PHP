464d6cd001d55c40bdd777baa8cb81b6c0ae8f6b
chytran <chytran@users.noreply.github.com>
2021-07-18 13:47:29 -0700

    Connecting login.php and login.inc.php

--- signup.php
+++ signup.php
@@ -11,7 +11,7 @@
                 <input type="text" name="email" placeholder="Email...">
                 <input type="text" name="uid" placeholder="Username...">
                 <input type="password" name="pwd" placeholder="Password...">
-                <input type="password" name="pwdRepeat" placeholder="Password...">
+                <input type="password" name="pwdRepeat" placeholder="Confirm Email...">
             
                 <button type="submit" name="submit">Sign Up</button>
             </form>
